2899b8399350049a1f263de0be4e4e18150faba0
Jose Antonio Barroso Clemente <j_antonio_bc@hotmail.com>
2018-10-03 08:43:46 +0200

    Agregar archivos de proyecto.

---
+++ RESTExercise/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RESTExercise.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
